c7d5f4da8dfc460e370df760c48f71f84ce9845f
HammadFKhan <hammadkn29@gmail.com>
2021-05-29 23:26:43 -0600

    Update kilosortPrep.m

--- Main/kilosortPrep.m
+++ Main/kilosortPrep.m
@@ -1,11 +1,11 @@
-function kilosortPrep(amplifier_data,path,filename)
+function kilosortPrep(amplifier_data,path)
 % Kilosort data preprocessing
 datI = int16(amplifier_data);
 kilosortOut = path;
-[SUCCESS,~,~] = mkdir(kilosortOut,filename(1:end-4));
-newDIR = dir(strcat(kilosortOut,filename(1:end-4)));
-newDIR = newDIR.folder;
-fid = fopen(strcat(newDIR,'/',filename(1:end-4),'.bin'),'w');
+% [SUCCESS,~,~] = mkdir(kilosortOut,filename(1:end-4));
+% DIR = dir(strcat(kilosortOut,filename(1:end-4)));
+% DIR = DIR.folder;
+fid = fopen(strcat(kilosortOut,'/','kilosort','.bin'),'w');
 fwrite(fid,datI,'int16');
 fclose(fid);
 disp('Kilosort Conversion Successful!')
