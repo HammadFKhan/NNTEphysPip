5ee922638e59b1bc99be548760d6318a08a5ec2a
HammadFKhan <hammadkn29@gmail.com>
2021-05-29 23:26:34 -0600

    Update batchLoad.m

--- Main/batchLoad.m
+++ Main/batchLoad.m
@@ -1,11 +1,12 @@
 %% Parallel Batch processing 
 function [pathname,VR_data] = batchLoad()
-pathname = strcat(uigetdir(pwd,'Input Directory'),'\');
+pathname = uigetdir(pwd,'Input Directory');
+pathname = fullfile(pathname);
 directory = dir(pathname);
 for idx = 1:length(directory)
-    fullfile = strcat(directory(idx).folder,'\',directory(idx).name)
+    filename = fullfile(directory(idx).folder,directory(idx).name)
     try
-        VR_data{idx} = table2array(readtable(fullfile));
+        VR_data{idx} = table2array(readtable(filename));
     catch ME
         VR_data{idx} = NaN;
         continue
