475f9094665d931a0a664dac7e49140c51bf863d
HammadFKhan <hammadkn29@gmail.com>
2021-06-02 23:19:26 -0600

    Fixed for trial analysis

--- Main/rateMap.m
+++ Main/rateMap.m
@@ -1,31 +1,34 @@
-function Spikes = rateMap(Spikes,VR_Data,varargin)
-if nargin<3 || strcmp(trialNumber,''), display('No Trial number was selected!') 
-end
+function Spikes = rateMap(Spikes,VR_data)
 
-p = inputParser;
-trialNumber = 3;
-
-addParameter(p,'trialNumber',trialNumber,@isnumeric);
-parse(p,Spikes,VR_Data,varargin{:});
-
-trialNumber = p.Results.trialNumber;
-
-position = VR_Data{1,trialNumber}(:,3);
-edgesPos = 0:2:200;
-VRposition = discretize(position,edgesPos);
-VRtime = VR_Data{1,trialNumber}(:,1);
-for i = 1:length(Spikes.Clusters)
-    for ii = 1:VRposition(end)
-        binFind = find(VRposition==ii);
-        map(ii,1) = min(binFind);
-        map(ii,2) = max(binFind);
-        mapTime(ii,1) = VRtime(map(ii,1));
-        mapTime(ii,2) = VRtime(map(ii,2));
-        spikeCount(ii,1) = length(find(mapTime(ii,1)<Spikes.Clusters(i).spikeTime...
-            & Spikes.Clusters(i).spikeTime<mapTime(ii,2)));
+for trial = 1:length(VR_data.Position)
+    position = VR_data.Position{trial};
+    edgesPos = 0:1:200; %Bin width on the track
+    VRposition = discretize(position,edgesPos);
+    VRtime = VR_data.Time{trial};
+    for i = 1:length(Spikes.Clusters)
+        for ii = 1:VRposition(end)
+            binFind = find(VRposition==ii);
+            map(ii,1) = min(binFind);
+            map(ii,2) = max(binFind);
+            
+            if trial-1 == 0
+                mapTime(ii,1) = VRtime(map(ii,1));
+                mapTime(ii,2) = VRtime(map(ii,2));
+            else
+                mapTime(ii,1) = VRtime(map(ii,1)) + max(VR_data.Time{1,trial-1}); %adds the previous trial if ther was one
+                mapTime(ii,2) = VRtime(map(ii,2)) + max(VR_data.Time{1,trial-1});
+            end
+            spikeCount(ii,1) = length(find(mapTime(ii,1)<Spikes.Clusters(i).spikeTime...
+                & Spikes.Clusters(i).spikeTime<mapTime(ii,2)));
+            
+        end
+        spikeRate(:,i) = spikeCount./(mapTime(:,2)-mapTime(:,1));
+        Spikes.VR(trial).spikeCount(:,i) = spikeCount;
     end
-    Spikes.VR.spikeCount(:,i) = spikeCount;
-    Spikes.VR.VR_data = VR_Data;
-    Spikes.VR.mapTime = mapTime;
-    Spikes.VR.position = VRposition;
+    
+    Spikes.VR(trial).time = VR_data.Time;
+    Spikes.VR(trial).mapTime = mapTime;
+    Spikes.VR(trial).position = VRposition;
+    Spikes.VR(trial).spikeRate = spikeRate;
+    clear mapTime spikeRate VRposition spikeCount
 end
