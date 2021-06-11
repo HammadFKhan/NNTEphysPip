fbd1b5f461619df98991f63fb01d391cc4cde8ac
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-06-10 20:33:52 -0400

    Update plotClusterless.m

--- Main/plotClusterless.m
+++ Main/plotClusterless.m
@@ -1,4 +1,4 @@
-function plotClusterless(Spikes,Ripples,filtData,Intan)
+function plotClusterless(Ripples,filtData,Intan)
 amplifier_data = Intan.amplifier_data;
 
 set(0,'DefaultFigureWindowStyle','docked')
@@ -6,28 +6,9 @@ addpath(genpath('Figures'));
 disp('Plotting...')
 
 try 
-figure,spikePlot = Show_Spikes(Spikes.binary);
-catch ME
-end
-
-try 
 figure('Name', 'Unfiltered Data'),stack_plot(amplifier_data);
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
     print(gcf,'-painters','-depsc', 'Figures/Raw_data.eps', '-r250');
-catch ME
-end
-
-try
-figure('Name','Singe Unit Waveforms'),SingleUnits(Spikes.allSpikes);
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-    print(gcf,'-painters','-depsc', 'Figures/Single_unit_waveforms.eps', '-r250');
-catch ME
-end
-
-try
-figure('Name','Multi-Unit Activity'),MUA(filtData.MUA);
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 4 3]);...
-    print(gcf,'-painters','-depsc', 'Figures/MultiUnit.eps', '-r250');
 catch ME
 end
 
