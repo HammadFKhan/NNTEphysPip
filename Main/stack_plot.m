c86b4b1477e4133877dc13e3643110cfd9205857
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-08-27 12:15:13 -0400

    Update stack_plot.m

--- Main/stack_plot.m
+++ Main/stack_plot.m
@@ -1,5 +1,5 @@
 function stack_plot(DeltaFoverF)
-Fs = 20000;
+Fs = 1024;
 
 x = length(DeltaFoverF(1,:));
 y = length(DeltaFoverF(:,1));
@@ -7,7 +7,7 @@ baseline = max(DeltaFoverF,[],'all');
 time = (1:x)/Fs;
 for i = 1:y
     gradient = i/y;
-    plot(time,1*DeltaFoverF(i,:)+(.7*baseline),'LineWidth',1,'Color',[.5 .5 .5 .8]); hold on;
+    plot(time,5*DeltaFoverF(i,:)+(.7*baseline),'LineWidth',1,'Color',[.5 .5 .5 .8]); hold on;
     baseline = baseline + max(DeltaFoverF,[],'all');
 end
 axis tight,box off
