c29c66d6220d20d60cddb207f6bbf224bbe2801c
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-08-27 12:14:43 -0400

    Fixed sample offset

--- IntanConcatenate.m
+++ IntanConcatenate.m
@@ -4,23 +4,31 @@ addpath(genpath('main'));
 pathname = uigetdir(pwd,'Input Directory');
 pathname = fullfile(pathname);
 directory = dir(pathname);
-
+Fs =  Intan.frequency_parameters.amplifier_sample_rate;
 count = 1;
-for idx = 3:20
+downsampleRate = 4;
+targetedFs = 8192;
+for idx = 3:63
     file = directory(idx).folder;
     path = directory(idx).name;
     Intan = read_Intan_RHD2000_file(file,path);   
-    allIntan{count} = Intan.amplifier_data(33:96,:);
+    allIntan{count} = resample(Intan.amplifier_data(33:96,:)',targetedFs,Fs);
     count = count+1;
 end % load Intan files
+Fs = Intan.frequency_parameters.amplifier_sample_rate;
 % Concatenate intan files for the whole session
-Intan.allIntan = horzcat(allIntan{:});
+disp('Combining...')
+Intan.allIntan = vertcat(allIntan{:})';
+disp('Compressing...')
 Intan.allIntan = single(Intan.allIntan);
+% Adjust electrode order by depth
+UCLA_probe_map
+Intan.allIntan  = Intan.allIntan(s.sorted_electrodes,:);
 % Fix recording offset
 Intan.offset = 1; % second
-Intan.offsetSample = Intan.frequency_parameters.amplifier_sample_rate*offset;
-disp(['Adjusting for ' offset 'second offset']);
-Intan.allIntan = Intan.allIntan(:,offsetSample:size(Intan.allIntan,2)-offsetSample);
+Intan.offsetSample = Intan.frequency_parameters.amplifier_sample_rate*Intan.offset;
+disp(['Adjusting for ' num2str(Intan.offset) ' second offset']);
+Intan.allIntan = Intan.allIntan(:,Intan.offsetSample:size(Intan.allIntan,2)-Intan.offsetSample);
 clear amplifier_data t_amplifier frequncy_parameters notes aux_input_channels...
     aux_input_data board_dig_in_channels board_dig_in_data amplifier_channels
 
