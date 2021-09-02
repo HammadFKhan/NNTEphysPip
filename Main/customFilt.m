3a779f8fb90bf2d277418ca636291f0dbca0138a
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-09-02 11:01:49 -0400

    Update customFilt.m

--- Main/customFilt.m
+++ Main/customFilt.m
@@ -1,12 +1,15 @@
-function customFilt(data,Fs,~)
+function filtered_data = customFilt(data,Fs,filtbound)
+if nargin < 3 || strcmp(filtbound,'')
+    error('Include cutoff frequencies [w1 w2]')
+end
 %% band-pass filtering
 % specify Nyquist freuqency
 nyquist = Fs/2;
 nbchan = size(data,1);
 pnts = size(data,2);
-times = 0:(pnts/Fs);
+times = (1:pnts)/Fs;
 % filter frequency band
-filtbound = [4 10]; % Hz
+% filtbound = [4 10]; % Hz
 
 % transition width
 trans_width = 0.2; % fraction of 1, thus 20%
@@ -24,25 +27,28 @@ idealresponse = [ 0 0 1 1 0 0 ];
 filterweights = firls(filt_order,ffrequencies,idealresponse);
 
 % plot for visual inspection
-figure(), clf
+figure(1), clf
 subplot(211)
 plot(ffrequencies*nyquist,idealresponse,'k--o','markerface','m')
 set(gca,'ylim',[-.1 1.1],'xlim',[-2 nyquist+2])
 xlabel('Frequencies (Hz)'), ylabel('Response amplitude')
-
+xlim([0 2*filtbound(2)])
 subplot(212)
 plot((0:filt_order)*(1000/Fs),filterweights)
-xlabel('Time (ms)'), ylabel('Amplitude')
+xlabel('Time (s)'), ylabel('Amplitude')
 
 % apply filter to data
 filtered_data = zeros(nbchan,pnts);
+disp('Applying Filter...')
+tic
 for chani=1:nbchan
-    filtered_data(chani,:) = filtfilt(filterweights,1,double(data(chani,:)));
+    filtered_data(chani,:) = filtfilt(filterweights,1,data(chani,:));
 end
+toc
 
-figure(), clf
-plot(times,squeeze(data(3,:)))
+figure(2), clf
+plot(times,squeeze(data(1,:)))
 hold on
-plot(times,squeeze(filtered_data(3,:)),'r','linew',2)
-xlabel('Time (ms)'), ylabel('Voltage (\muV)')
+plot(times,squeeze(filtered_data(1,:)),'r','linew',2)
+xlabel('Time (s)'), ylabel('Voltage (\muV)')
 legend({'raw data';'filtered'})
