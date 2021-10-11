149e07319bdbdf1b91d930036acf30ad58a8c6ff
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-10-11 12:20:20 -0400

    Update bestLFP.m

--- Main/bestLFP.m
+++ Main/bestLFP.m
@@ -4,7 +4,7 @@ pow = rms(LFP.LFP,2);
 [~,loc] = max(pow);
 disp(['Best LFP: ' num2str(loc)])
 count = 1;
-for chan = 1:size(LFP,1)
+for chan = 1:size(LFP.LFP,1)
     rho = corr(LFP.LFP(loc,:)',LFP.LFP(chan,:)');
     if rho>0.9
         medianLFP(count,:) = LFP.LFP(chan,:);
@@ -12,7 +12,10 @@ for chan = 1:size(LFP,1)
     end
 end
 disp('Resampling to original Frequency');
+LFP.medianLFP = medianLFP;
 LFP.bestLFP = median(medianLFP,1); %Best LFP for Phase Phase Analysis
+commonModeAvg = medianLFP-mean(medianLFP);
+filtData.commonModeAvg = commonModeAvg;
 LFP.coherenceLFP = resample(LFP.bestLFP,8192,1024); %Spike coherence LFP
 LFP.times = (1:size(LFP.bestLFP,2))/LFP.downSampleFreq;
 
