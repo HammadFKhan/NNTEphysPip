a5a97b0c426916390823ada4616f320456e1f896
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-17 22:08:13 -0500

    Update tfAnalysis.m

--- Main/tfAnalysis.m
+++ Main/tfAnalysis.m
@@ -23,8 +23,8 @@ downsample_LFPTime = LFP.times';
 
 % Set up state triggered analysis
 Velocity = Spikes.VR.Velocity(:,2);
-velocityTrig = 1.2*mean(Velocity);
-loc = 3*find(Velocity>velocityTrig); %multiply by the cause of bin value
+velocityTrig = 1; %Triggered Velocity
+loc = 3*find(abs(Velocity)>velocityTrig); %multiply by the cause of bin value
 for trial = 1:length(loc)-1
     window = (loc(trial)-1.00:loc(trial)+1.00);
     %triggered spike time with offset of the intial spike
@@ -51,25 +51,25 @@ params.fpass = [30 80];
 
 params.Fs = 8192;
 % Theta Band Analysis
-% disp('Analyzing Theta Band...')
-% params.fpass = [4 10];
+disp('Analyzing Theta Band...')
+params.fpass = [4 10];
 % [tf.theta.X,tf.t,tf.theta.f]= mtspecgramc(LFPTrig,movingwin,params); 
-% [tf.theta.C,tf.theta.phi,S12,S1,S2,t2,f2,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params);
+[tf.theta.C,tf.theta.phi,S12,S1,S2,t2,tf.theta.f,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params);
 % tf.mtheta = mean(tf.theta.X,3);
 % tf.metheta = median(tf.theta.X,3);
 
-% params.fpass = [10 30];
-% % Beta Band Analysis
-% disp('Analyzing Beta Band...')
+params.fpass = [10 30];
+% Beta Band Analysis
+disp('Analyzing Beta Band...')
 % [tf.beta.X,t,tf.beta.f] = mtspecgramc(LFPTrig,movingwin,params); 
-% [tf.beta.C,tf.beta.phi,S12,S1,S2,t2,f2,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params); 
+[tf.beta.C,tf.beta.phi,S12,S1,S2,t2,tf.beta.f,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params); 
 % tf.mbeta = mean(tf.beta.X,3);
 % tf.mebeta = median(tf.beta.X,3);
-% params.fpass = [30 80];
-% % Gamma Band Analysis
-% disp('Analyzing Gamma Band...')
+params.fpass = [30 80];
+% Gamma Band Analysis
+disp('Analyzing Gamma Band...')
 % [tf.gamma.X,t,tf.gamma.f] = mtspecgramc(LFPTrig,movingwin,params);
-% [tf.gamma.C,tf.gamma.phi,S12,S1,S2,t2,f2,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params);
+[tf.gamma.C,tf.gamma.phi,S12,S1,S2,t2,tf.gamma.f,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params);
 % tf.mgamma = mean(tf.gamma.X,3);
 % tf.megamma = median(tf.gamma.X,3);
 % % Broadband
@@ -79,14 +79,26 @@ params.Fs = 8192;
 % [tf.broad.C,tf.broad.phi,S12,S1,S2,t2,f2,zerosp]=cohgramcpt(LFPTrig,spike,movingwin,params);
 % tf.mbroad = mean(tf.broad.X,3);
 % tf.mebroad = median(tf.broad.X,3);
+
 % Inter-trial Phase Clustering
-% tf.theta.theta = tf.theta.phi(:,2,:);
-% tf.beta.beta = tf.beta.phi(:,2,:);
-% tf.gamma.gamma = tf.gamma.phi(:,2,:);
-% 
-% tf.theta.itpc = abs(mean(exp(1i*tf.theta.theta)));
-% tf.beta.itpc = abs(mean(exp(1i*tf.beta.beta)));
-% tf.gamma.itpc = abs(mean(exp(1i*tf.gamma.gamma)));
+C = permute(tf.theta.phi,[1 3 2]);
+C = reshape(C,[],size(tf.theta.phi,2),1);
+C1 = permute(tf.beta.phi,[1 3 2]);
+C1 = reshape(C1,[],size(tf.beta.phi,2),1);
+C2 = permute(tf.gamma.phi,[1 3 2]);
+C2 = reshape(C2,[],size(tf.gamma.phi,2),1);
+for j = 1:size(tf.theta.phi,2)
+    tf.theta.theta{j} = C(:,j);
+    tf.theta.itpc(j,1) = abs(mean(exp(1i*tf.theta.theta{j})));
+end
+for j = 1:size(tf.beta.phi,2)
+    tf.beta.beta{j} = C1(:,j);
+    tf.beta.itpc(j,1) = abs(mean(exp(1i*tf.beta.beta{j})));
+end
+for j = 1:size(tf.gamma.phi,2)
+    tf.gamma.gamma{j} = C2(:,j);
+    tf.gamma.itpc(j,1) = abs(mean(exp(1i*tf.gamma.gamma{j})));
+end
 disp('Performing Wavelet Analysis...')
 for trial = 1:size(LFPTrig,2)
     [tf.wavelet.cfs(:,:,trial),tf.wavelet.f] = cwt(LFPTrig(:,trial),1024,'FrequencyLimits',[0 80]);
