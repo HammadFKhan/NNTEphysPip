47e3441fac39a14e047d5e9e2c35d58f77a42e58
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-17 22:07:02 -0500

    Update ISI.m

--- Main/ISI.m
+++ Main/ISI.m
@@ -10,7 +10,7 @@ for i = 1:length(Spikes.Clusters)
     Spikes.Clusters(i).spikeTime = cluster;
     subplot(sizePlot,sizePlot,i),histogram(Spikes.Clusters(i).ISI,'BinWidth',Interval,'FaceColor',[0 0 0]),axis tight,box off;
     title(['Cluster ' num2str(i)]);
-    set(gca,'YTick','','YTickLabel','');
+%     set(gca,'YTick','','YTickLabel','');
     xlabel('Time (s)')
     xlim([0 1]);
 end
