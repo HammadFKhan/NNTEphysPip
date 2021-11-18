9cfd0748df3f8cb0ca1c97eb9c4fe68156fd9c5b
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-17 22:06:42 -0500

    Update bestLFP.m

--- Main/bestLFP.m
+++ Main/bestLFP.m
@@ -4,9 +4,9 @@ pow = rms(LFP.LFP,2);
 [~,loc] = max(pow);
 disp(['Best LFP: ' num2str(loc)])
 count = 1;
-for chan = 1:size(LFP,1)
+for chan = 1:size(LFP.LFP,1)
     rho = corr(LFP.LFP(loc,:)',LFP.LFP(chan,:)');
-    if rho>0.9
+    if rho>0.7
         medianLFP(count,:) = LFP.LFP(chan,:);
         count = count+1;
     end
