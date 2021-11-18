5179c8a07f5314bfd48c5c986c3767686e24c511
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2021-11-17 22:07:11 -0500

    Update LFPplot.m

--- Main/LFPplot.m
+++ Main/LFPplot.m
@@ -1,16 +1,18 @@
 function LFPplot(LFP)
 Fs = LFP.downSampleFreq;
-win = 1;
-figure(3),
+start = 586*Fs;
+win = 5*Fs;
+stop = start+win-1;
+figure(3),set(gcf, 'Position',  [100, 100, 400, 500]),
  %Plot the first 5 seconds of data
-subplot(4,1,1),plot(LFP.times(:,1:(win*Fs)),LFP.bestLFP(:,1:(win*Fs)),'k'); title('Raw Data'),box off
-subplot(4,1,2),plot(LFP.times(:,1:(win*Fs)),LFP.theta_band(:,1:(win*Fs)),'r');title('\theta Band'),box off
-subplot(4,1,3),plot(LFP.times(:,1:(win*Fs)),LFP.beta_band(:,1:(win*Fs)),'m');title('\beta Band'),box off
-subplot(4,1,4),plot(LFP.times(:,1:(win*Fs)),LFP.gamma_band(:,1:(win*Fs)),'g');title('\gamma Band'),box off
+subplot(4,1,1),plot(LFP.times(:,1:win),LFP.LFP(1,start:stop),'k'); title('Raw Data'),box off,ylim([-300 300])
+subplot(4,1,2),plot(LFP.times(:,1:win),LFP.theta_band(:,start:stop),'r');title('\theta Band'),box off,ylim([-100 100])
+subplot(4,1,3),plot(LFP.times(:,1:win),LFP.beta_band(:,start:stop),'m');title('\beta Band'),box off,ylim([-100 100])
+subplot(4,1,4),plot(LFP.times(:,1:win),LFP.gamma_band(:,start:stop),'g');title('\gamma Band'),box off,ylim([-100 100])
 xlabel('Time (s)'), ylabel('Voltage (\muV)')
 
 figure(4)
-subplot(3,1,1),plot(LFP.times(:,1:(win*Fs)),LFP.theta_temppow(:,1:(win*Fs)),'r');title('\theta Band'),box off
-subplot(3,1,2),plot(LFP.times(:,1:(win*Fs)),LFP.beta_temppow(:,1:(win*Fs)),'m');title('\beta Band'),box off
-subplot(3,1,3),plot(LFP.times(:,1:(win*Fs)),LFP.beta_temppow(:,1:(win*Fs)),'g');title('\gamma Band'),box off
+subplot(3,1,1),plot(LFP.times(:,1:win),LFP.theta_temppow(:,start:stop),'r');title('\theta Band'),box off
+subplot(3,1,2),plot(LFP.times(:,1:win),LFP.beta_temppow(:,start:(stop)),'m');title('\beta Band'),box off
+subplot(3,1,3),plot(LFP.times(:,1:win),LFP.beta_temppow(:,start:stop),'g');title('\gamma Band'),box off
 xlabel('Time (s)'), ylabel('Power (dB)')
