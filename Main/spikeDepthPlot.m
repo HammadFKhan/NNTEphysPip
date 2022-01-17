5b48c77aaf28a82351e0a4683a4c3d245600505f
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-01-17 14:31:03 -0500

    Update spikeDepthPlot.m

--- Main/spikeDepthPlot.m
+++ Main/spikeDepthPlot.m
@@ -1,7 +1,9 @@
 function spikeDepthPlot(Spikes,templateDepths)
-temp = templateDepths*2*25;
+temp = templateDepths(1:length(Spikes.Clusters))*25;
 % find max cluster size
-clusterMax = max(Spikes.Clusters.spikeTime)
+[idx,loc] = sort(temp);
+norm = (Spikes.VR.spikeRate-min(spikeRate,[],1))./(max(Spikes.VR.spikeRate,[],1)-min(Spikes.VR.spikeRate,[],1));
+imagesc(norm(:,loc)),colormap(jet)
 figure,
 for i = 1:5
     disp(i)
