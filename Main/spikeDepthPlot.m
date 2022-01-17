320f8eb6205844c241df57d3591f02af877db81f
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-01-17 16:27:05 -0500

    Update spikeDepthPlot.m

--- Main/spikeDepthPlot.m
+++ Main/spikeDepthPlot.m
@@ -1,11 +1,25 @@
-function spikeDepthPlot(Spikes,templateDepths)
+function sortedSpikeRate = spikeDepthPlot(Spikes,templateDepths)
 temp = templateDepths(1:length(Spikes.Clusters))*25;
 % find max cluster size
-[idx,loc] = sort(temp);
-norm = (Spikes.VR.spikeRate-min(spikeRate,[],1))./(max(Spikes.VR.spikeRate,[],1)-min(Spikes.VR.spikeRate,[],1));
-imagesc(norm(:,loc)),colormap(jet)
-figure,
-for i = 1:5
-    disp(i)
-    plot(Spikes.Clusters(i).spikeTime,i,'.'),hold on;
+[depth,idx] = sort(temp);
+
+Velocity = Spikes.VR.Velocity(:,2);
+velocityTrig = 1; %Triggered at 1 cm
+loc = find(abs(Velocity)>=velocityTrig);
+sortedSpikeRate = Spikes.VR.spikeRate(:,idx)';
+
+figure
+minmax = (sortedSpikeRate-min(sortedSpikeRate,[],1))./(max(sortedSpikeRate,[],1)-min(sortedSpikeRate,[],1));
+imagesc(1:100,depth,smoothdata(minmax,'gaussian',10)),colormap(jet),colorbar
+
+for i  = 1:length(Spikes.Clusters)
+    clustSize(i) = size(Spikes.Clusters(i).spikeTime,2);
+end
+spikeRaster = zeros(length(clustSize),max(clustSize));
+for i = 1:length(Spikes.Clusters)
+    spikeCell = Spikes.Clusters(i).spikeTime;
+    pad = abs(length(spikeCell)-max(clustSize));
+    spikeRaster(i,:) = [spikeCell zeros(1,pad)];
+end
+figure,plot(spikeRaster,'.'),view([90 -90]);
 end
