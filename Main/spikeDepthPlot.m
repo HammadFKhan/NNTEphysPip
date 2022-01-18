0da66a62735a40124a2cf3728374f08422109d16
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-01-17 19:51:46 -0500

    Update spikeDepthPlot.m

--- Main/spikeDepthPlot.m
+++ Main/spikeDepthPlot.m
@@ -1,4 +1,4 @@
-function sortedSpikeRate = spikeDepthPlot(Spikes,templateDepths)
+function Spikes = spikeDepthPlot(Spikes,templateDepths)
 temp = templateDepths(1:length(Spikes.Clusters))*25;
 % find max cluster size
 [depth,idx] = sort(temp);
@@ -21,5 +21,12 @@ for i = 1:length(Spikes.Clusters)
     pad = abs(length(spikeCell)-max(clustSize));
     spikeRaster(i,:) = [spikeCell zeros(1,pad)];
 end
-figure,plot(spikeRaster,'.'),view([90 -90]);
+spikeRaster(spikeRaster==0) = nan;
+figure,plot(spikeRaster,'.','color','k'),view([90 -90]); 
+ylim([0 60])
+% Save output
+Spikes.Depth.depth = depth;
+Spikes.Depth.clusterID = idx;
+Spikes.Depth.sortedSpikeRate = sortedSpikeRate;
+Spikes.Depth.spikeRaster = spikeRaster;
 end
