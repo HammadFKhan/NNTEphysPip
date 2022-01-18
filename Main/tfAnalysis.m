0fbf6f89947d32dff4bacef2844a2bc4f0af4301
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-01-17 19:51:56 -0500

    Update tfAnalysis.m

--- Main/tfAnalysis.m
+++ Main/tfAnalysis.m
@@ -10,7 +10,26 @@ TimeFreq = [];
 for trial = 1:size(Spikes.Clusters,2)
     fixSpiketime{trial} = Spikes.Clusters(trial).spikeTime';
 end
-spikeTime = sort(vertcat(fixSpiketime{:}));
+spikeTime = sort(vertcat(fixSpiketime{:})); %% All spike times of neurons
+% Set up depth specific spike data
+L23 = find(Spikes.Depth.depth<250);
+L4 = find(Spikes.Depth.depth>250 & Spikes.Depth.depth<400);
+L5 = find(Spikes.Depth.depth>400);
+% Now build spike rasters and time for each layer
+clusterID = Spikes.Depth.clusterID;
+l23 = {};l4 = {};l5 = {};
+for i = 1:length(L23)
+    l23{i} = Spikes.Clusters(clusterID(L23(i))).spikeTime'; %match electrode ID and layer location
+end
+for i = 1:length(L4)
+    l4{i} = Spikes.Clusters(clusterID(L4(i))).spikeTime'; %match electrode ID and layer location
+end
+for i = 1:length(L5)
+    l5{i} = Spikes.Clusters(clusterID(L5(i))).spikeTime'; %match electrode ID and layer location
+end
+L23spikeTime = sort(vertcat(l23{:}));
+L4spikeTime = sort(vertcat(l4{:}));
+L5spikeTime = sort(vertcat(l5{:}));
 
 coherencyLFP =LFP.coherenceLFP'; % Reference LFP for spiking data (matched Fs) 
 thetaLFP = LFP.theta_band'; %Downsampled LFP for P:P analysis
@@ -23,10 +42,11 @@ downsample_LFPTime = LFP.times';
 
 % Set up state triggered analysis
 Velocity = Spikes.VR.Velocity(:,2);
-velocityTrig = 1; %Triggered Velocity
+velocityTrig = 2; %Triggered Velocity
 loc = Spikes.VR.binWinTime*find(abs(Velocity)>velocityTrig); %multiply by the cause of bin value
 
 % Check and adjust data structure for depth or single electrode analysis
+disp('Constructing Spike Windows...')
 if size(thetaLFP,1)>1
     for trial = 1:length(loc)-1
         window = [loc(trial)-1.50,loc(trial)+.500];
@@ -34,6 +54,10 @@ if size(thetaLFP,1)>1
         %this way, each spike time is translated to a window within 1 second
         timestamps(trial,:) = window; % Global timestamps for signal reference
         spike(trial).spikeTrig = spikeTime(window(1)<=spikeTime & spikeTime<=window(2))-window(1);
+        % Create spike window for each layer
+        L23spike(trial).spikeTrig = L23spikeTime(window(1)<=L23spikeTime & L23spikeTime<=window(2))-window(1);
+        L4spike(trial).spikeTrig = L4spikeTime(window(1)<=L4spikeTime & L4spikeTime<=window(2))-window(1);
+        L5spike(trial).spikeTrig = L5spikeTime(window(1)<=L5spikeTime & L5spikeTime<=window(2))-window(1);
         LFPTrig(:,:,trial) = coherencyLFP(window(1)<=LFPTime & LFPTime<=window(2));
         theta(:,:,trial)= thetaLFP(window(1)<=downsample_LFPTime & downsample_LFPTime<=window(2),:);
         beta(:,:,trial)= betaLFP(window(1)<=downsample_LFPTime & downsample_LFPTime<=window(2),:);
@@ -67,9 +91,11 @@ LFP.betaTrials = beta;
 TimeFreq.mgamma = mean(gamma,2);
 
 %% ITPC of velocity triggered Frequency
+disp('Calculating Electrode ITPC')
 phaseSync = itpc(LFP,timestamps,10);
 %% Oscillators Analysis using Chronux
 % Phase to Phase of Theta and Beta
+disp('Calculating LFP Phase Coupling')
 params.Fs = 1024;
 params.fpass = [4 30];
 [tf.oscillators.tb.C,tf.oscillators.tb.phi,S12,S1,S2,tf.oscillators.t,tf.oscillators.tb.f]=cohgramc(theta,beta,movingwin,params);
@@ -77,8 +103,6 @@ params.fpass = [4 80];
 [tf.oscillators.tg.C,tf.oscillators.tg.phi,S12,S1,S2,t,tf.oscillators.tg.f]=cohgramc(theta,gamma,movingwin,params);
 params.fpass = [30 80];
 [tf.oscillators.bg.C,tf.oscillators.bg.phi,S12,S1,S2,t,tf.oscillators.bg.f]=cohgramc(beta,gamma,movingwin,params);
-
-TimeFreq.tf = tf;
 
 params.Fs = 8192;
 % Theta Band Analysis
@@ -111,25 +135,49 @@ disp('Analyzing Gamma Band...')
 % tf.mbroad = mean(tf.broad.X,3);
 % tf.mebroad = median(tf.broad.X,3);
 
-% Inter-trial Phase Clustering of Spike-LFP
-C = permute(tf.theta.phi,[1 3 2]);
-C = reshape(C,[],size(tf.theta.phi,2),1);
-C1 = permute(tf.beta.phi,[1 3 2]);
-C1 = reshape(C1,[],size(tf.beta.phi,2),1);
-C2 = permute(tf.gamma.phi,[1 3 2]);
-C2 = reshape(C2,[],size(tf.gamma.phi,2),1);
-for j = 1:size(tf.theta.phi,2) %ITPC for each frequency
-    tf.theta.theta{j} = C(:,j);
-    tf.theta.itpc(j,1) = abs(mean(exp(1i*tf.theta.theta{j})));
-end
-for j = 1:size(tf.beta.phi,2) %ITPC for each frequency
-    tf.beta.beta{j} = C1(:,j);
-    tf.beta.itpc(j,1) = abs(mean(exp(1i*tf.beta.beta{j})));
-end
-for j = 1:size(tf.gamma.phi,2)
-    tf.gamma.gamma{j} = C2(:,j);
-    tf.gamma.itpc(j,1) = abs(mean(exp(1i*tf.gamma.gamma{j})));
-end
+% Depth analysis of spikes
+% Theta Band Analysis
+disp('Analyzing Theta Band for Layer 2/3...')
+params.fpass = [4 10];
+[tf.depth.L23.theta.C,tf.depth.L23.theta.phi,S12,S1,S2,t2,tf.theta.f,zerosp]=cohgramcpt(LFPTrig,L23spike,movingwin,params);
+disp('Analyzing Theta Band for Layer 4...')
+[tf.depth.L4.theta.C,tf.depth.L23.theta.phi,S12,S1,S2,t2,tf.theta.f,zerosp]=cohgramcpt(LFPTrig,L4spike,movingwin,params);
+disp('Analyzing Theta Band for Layer 5...')
+[tf.depth.L5.theta.C,tf.depth.L23.theta.phi,S12,S1,S2,t2,tf.theta.f,zerosp]=cohgramcpt(LFPTrig,L5spike,movingwin,params);
+
+% Beta Band Analysis
+params.fpass = [10 30];
+disp('Analyzing Beta Band for Layer 2/3...')
+[tf.depth.L23.beta.C,tf.depth.L23.beta.phi,S12,S1,S2,t2,tf.beta.f,zerosp]=cohgramcpt(LFPTrig,L23spike,movingwin,params);
+disp('Analyzing Beta Band for Layer 4...')
+[tf.depth.L4.beta.C,tf.depth.L23.beta.phi,S12,S1,S2,t2,tf.beta.f,zerosp]=cohgramcpt(LFPTrig,L4spike,movingwin,params);
+disp('Analyzing Beta Band for Layer 5...')
+[tf.depth.L5.beta.C,tf.depth.L23.beta.phi,S12,S1,S2,t2,tf.beta.f,zerosp]=cohgramcpt(LFPTrig,L5spike,movingwin,params);
+
+% Gamma Band Analysis
+params.fpass = [30 80];
+disp('Analyzing Gamma Band for Layer 2/3...')
+[tf.depth.L23.gamma.C,tf.depth.L23.gamma.phi,S12,S1,S2,t2,tf.gamma.f,zerosp]=cohgramcpt(LFPTrig,L23spike,movingwin,params);
+disp('Analyzing Gamma Band for Layer 4...')
+[tf.depth.L4.gamma.C,tf.depth.L23.gamma.phi,S12,S1,S2,t2,tf.gamma.f,zerosp]=cohgramcpt(LFPTrig,L4spike,movingwin,params);
+disp('Analyzing Gamma Band for Layer 5...')
+[tf.depth.L5.gamma.C,tf.depth.L23.gamma.phi,S12,S1,S2,t2,tf.gamma.f,zerosp]=cohgramcpt(LFPTrig,L5spike,movingwin,params);
+
+%% Inter-trial Phase Clustering of Spike-LFP
+[tf.theta.theta,tf.theta.itpc,tf.beta.beta,tf.beta.itpc,tf.gamma.gamma,tf.gamma.itpc] = chronuxITPC(tf.theta.phi,tf.beta.phi,tf.gamma.phi);
+
+%% Inter-trial Phase Clustering of Spike-LFP per Layer
+disp('ITPC for layer 2/3')
+[tf.depth.L23.theta.theta,tf.depth.L23.theta.itpc,tf.depth.L23.beta.beta,tf.depth.L23.beta.itpc,tf.depth.L23.gamma.gamma,tf.depth.L23.gamma.itpc]...
+    = chronuxITPC(tf.depth.L23.theta.phi,tf.depth.L23.beta.phi,tf.depth.L23.gamma.phi);
+disp('ITPC for layer 4')
+[tf.depth.L4.theta.theta,tf.depth.L4.theta.itpc,tf.depth.L4.beta.beta,tf.depth.L4.beta.itpc,tf.depth.L4.gamma.gamma,tf.depth.L4.gamma.itpc]...
+    = chronuxITPC(tf.depth.L4.theta.phi,tf.depth.L4.beta.phi,tf.depth.L4.gamma.phi);
+disp('ITPC for layer 5')
+[tf.depth.L5.theta.theta,tf.depth.L5.theta.itpc,tf.depth.L5.beta.beta,tf.depth.L5.beta.itpc,tf.depth.L5.gamma.gamma,tf.depth.L5.gamma.itpc]...
+    = chronuxITPC(tf.depth.L5.theta.phi,tf.depth.L5.beta.phi,tf.depth.L5.gamma.phi);
+
+
 % disp('Performing Wavelet Analysis...')
 % for trial = 1:size(LFPTrig,2)
 %     [tf.wavelet.cfs(:,:,trial),tf.wavelet.f] = cwt(LFPTrig(:,trial),1024,'FrequencyLimits',[0 80]);
