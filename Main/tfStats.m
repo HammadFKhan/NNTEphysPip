b4eb4deb6cd6508fda3a283927192afda27e4527
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-01-19 23:45:45 -0500

    Create tfStats.m

---
+++ Main/tfStats.m
@@ -0,0 +1,42 @@
+function stats = tfStats(TimeFreq)
+L23 = TimeFreq.tf.depth.L23;
+L4 = TimeFreq.tf.depth.L4;
+L5 = TimeFreq.tf.depth.L5;
+
+layersTheta = [L23.theta.itpc;L4.theta.itpc;L5.theta.itpc];
+xTheta = [repmat({'L2/3 theta'},length(L23.theta.itpc), 1); repmat({'L4-theta'},length(L4.theta.itpc), 1); repmat({'L5-theta'},length(L5.theta.itpc), 1)];
+layersBeta = [L23.beta.itpc;L4.beta.itpc;L5.beta.itpc];
+xBeta = [repmat({'L2/3-beta'},length(L23.beta.itpc), 1); repmat({'L4-beta'},length(L4.beta.itpc), 1); repmat({'L5-beta'},length(L5.beta.itpc), 1)];
+layersGamma = [L23.gamma.itpc;L4.gamma.itpc;L5.gamma.itpc];
+xGamma = [repmat({'L2/3-gamma'},length(L23.gamma.itpc), 1); repmat({'L4-gamma'},length(L4.gamma.itpc), 1); repmat({'L5-gamma'},length(L5.gamma.itpc), 1)];
+
+layers = [layersTheta; layersBeta; layersGamma];
+x = [xTheta; xBeta; xGamma];
+
+figure,boxplot(layers,x,'PlotStyle','compact')
+
+layers23 = [L23.theta.itpc;L23.beta.itpc;L23.gamma.itpc];
+x23 = [repmat({'L2/3-theta'},length(L23.theta.itpc), 1); repmat({'L23-beta'},length(L23.beta.itpc), 1); repmat({'L23-gamma'},length(L23.gamma.itpc), 1)];
+layers4 = [L4.theta.itpc;L4.beta.itpc;L4.gamma.itpc];
+x4 = [repmat({'L4-theta'},length(L4.theta.itpc), 1); repmat({'L4-beta'},length(L4.beta.itpc), 1); repmat({'L4-gamma'},length(L4.gamma.itpc), 1)];
+layers5 = [L5.theta.itpc;L5.beta.itpc;L5.gamma.itpc];
+x5 = [repmat({'L5-theta'},length(L5.theta.itpc), 1); repmat({'L5-beta'},length(L5.beta.itpc), 1); repmat({'L5-gamma'},length(L5.gamma.itpc), 1)];
+
+freq = [layers23; layers4; layers5];
+xl = [x23; x4; x5];
+
+figure,boxplot(freq,xl,'PlotStyle','compact')
+
+% Export stat structure
+stats.layersTheta = layersTheta;
+stats.xTheta = xTheta;
+stats.xBeta = xBeta;
+stats.xGamma = xGamma;
+stats.layers = layers;
+stats.x = x;
+
+stats.layers23 = layers23;
+stats.layers4 = layers4;
+stats.layers5 = layers5;
+stats.freq = freq;
+stats.xl = xl;
