1e7cbc4091ba615da751e898610ce1db86dafa2f
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-04-12 13:49:07 -0400

    Update gammaBetaCoupling.m

--- Main/gammaBetaCoupling.m
+++ Main/gammaBetaCoupling.m
@@ -1,18 +1,35 @@
 function betaGammaCoupling = gammaBetaCoupling(mGamma,mBeta)
-%% Gamma amplitude phase coupling
-figure,hold on
-for ii = 1:11
-mGammaAmplitude = mGamma(:,ii).^2;
-mBetaPhase = angle(hilbert(mBeta(:,ii)));
-%discretize the angle into 18 equal bins
-dBetaPhase = discretize(mBetaPhase*180/pi,-180:20:180);
-% Sort gamma into beta bins
-for i = 1:max(dBetaPhase)
-    betaGammaCoupling(i) = mean(mGammaAmplitude(dBetaPhase==i,1));
+%% Gamma amplitude phase coupling by depth and trial
+if size(mGamma) & size(mBeta) > 2 % Beta coupling by depth
+    for ii = 1:size(mBeta,2)
+%         figure,hold on
+        for j = 1:size(mBeta,3)
+        mGammaAmplitude = mGamma(:,ii,j).^2;
+        mBetaPhase = angle(hilbert(mBeta(:,ii,j)));
+        %discretize the angle into 18 equal bins
+        dBetaPhase = discretize(mBetaPhase*180/pi,-180:20:180);
+        % Sort gamma into beta bins
+        for i = 1:max(dBetaPhase)
+            betaGammaCoupling(i,ii,j) = median(mGammaAmplitude(dBetaPhase==i,1));
+        end
+%         bar(-179:20:180,betaGammaCoupling)
+%         xlim([-180 180])
+        end
+    end
+else
+    for ii = 1:size(mBeta,2)
+        mGammaAmplitude = mGamma(:,ii).^2;
+        mBetaPhase = angle(hilbert(mBeta(:,ii)));
+        %discretize the angle into 18 equal bins
+        dBetaPhase = discretize(mBetaPhase*180/pi,-180:20:180);
+        % Sort gamma into beta bins
+        for i = 1:max(dBetaPhase)
+            betaGammaCoupling(i) = mean(mGammaAmplitude(dBetaPhase==i,1));
+        end
+        bar(-179:20:180,betaGammaCoupling)
+    end
+    xlim([-180 180])
 end
-bar(-179:20:180,betaGammaCoupling)
-end
-xlim([-180 180])
 %% Random Plots
 
 % data = mGamma(:,1);
