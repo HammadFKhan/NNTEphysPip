34b5261bfe6551ece15428d2a894fbb276244cf4
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-04-19 17:31:45 -0400

    Update spikeRaster.m

--- Main/spikeRaster.m
+++ Main/spikeRaster.m
@@ -5,27 +5,27 @@ spikeTemp = vertcat(spikeTemp{:});
 figure,hold on
 for i = 1:size(spikeTemp,1)
     r = horzcat(spikeTemp(i,:));
-    raster{i} = vertcat(r{:});
-    plot(raster{i},(i/5)*ones(size(raster{i},1)),'.')
+    raster = vertcat(r{:});
+    plot(raster,i*ones(size(raster,1)),'.')
 end
 
-spikeTemp = squeeze(struct2cell(Spikes.spikes.L4Run)); % Spike temp for layer 23 of cell cell array
-spikeTemp = vertcat(spikeTemp{:});
-figure,hold on
-for i = 1:size(spikeTemp,1)
-    r = horzcat(spikeTemp(i,:));
-    raster{i} = vertcat(r{:});
-    plot(raster{i},(i/5)*ones(size(raster{i},1)),'.')
-end
-spikeTemp = [];
+% spikeTemp = squeeze(struct2cell(Spikes.spikes.L4Run)); % Spike temp for layer 23 of cell cell array
+% spikeTemp = vertcat(spikeTemp{:});
+% figure,hold on
+% for i = 1:size(spikeTemp,1)
+%     r = horzcat(spikeTemp(i,:));
+%     raster{i} = vertcat(r{:});
+%     plot(raster{i},(i/5)*ones(size(raster{i},1)),'.')
+% end
+% spikeTemp = [];
 
 spikeTemp = squeeze(struct2cell(Spikes.spikes.L5Run)); % Spike temp for layer 23 of cell cell array
 spikeTemp = vertcat(spikeTemp{:});
 figure,hold on
 for i = 1:size(spikeTemp,1)
     r = horzcat(spikeTemp(i,:));
-    raster{i} = vertcat(r{:});
-    plot(raster{i},(i/5)*ones(size(raster{i},1)),'.')
+    raster = vertcat(r{:});
+    plot(raster,i*ones(size(raster,1)),'.')
 end
 spikeTemp = [];
 end
