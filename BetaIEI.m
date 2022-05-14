4b1b5e6a37836412a8d42a9aa9c632a4f67440e0
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-05-13 22:00:42 -0400

    Update BetaIEI.m

--- BetaIEI.m
+++ BetaIEI.m
@@ -20,18 +20,19 @@ for ii = 1:30
             %         B = win(win>1);
             %         ERD(i) = (length(A)-length(B))/(length(A)+length(B));
             %Beta Event Interval
-            L23EventISI{i} = abs(diff(win));
-            L23RunEventISI{i} = abs(diff(win(win>=1))); % Run Window
-            L23InitiateEventISI{i} = abs(diff(win(win<1))); % Initiate Window
-            if isempty(L23RunEventISI{i}) && isempty(L23InitiateEventISI{i})
-                L23RunEventISI{i} = -.5;
-                L23InitiateEventISI{i} = -.5;
-            elseif isempty(L23RunEventISI{i}) 
-                L23RunEventISI{i} = -.5;
-            elseif isempty(L23InitiateEventISI{i})
-                L23InitiateEventISI{i} = -.5;
+            L23EventISI{count} = abs(diff(win));
+            L23RunEventISI{count} = abs(diff(win(win>=1))); % Run Window
+            L23InitiateEventISI{count} = abs(diff(win(win<1))); % Initiate Window
+            if isempty(L23RunEventISI{count}) && isempty(L23InitiateEventISI{count})
+                L23RunEventISI{count} = -.5;
+                L23InitiateEventISI{count} = -.5;
+            elseif isempty(L23RunEventISI{count}) 
+                L23RunEventISI{count} = -.5;
+            elseif isempty(L23InitiateEventISI{count})
+                L23InitiateEventISI{count} = -.5;
             end
         end
+        count = count+1;
     end
 end
 
@@ -40,13 +41,14 @@ L23InitiateEventISI = vertcat(L23InitiateEventISI{:});
 L23RunEventISI = vertcat(L23RunEventISI{:});
 
 figure('Name','Layer 2/3'),histogram(L23EventISI,0:.1:2)
-figure('Name','Initiate Layer 2/3'),yyaxis left,histogram(L23InitiateEventISI,0:.1:2),title('Initiate Layer2/3'),ylim([0 45])
-yyaxis right,histogram(L23InitiateEventISI(L23InitiateEventISI==-0.5),-1:0.1:0),ylim([0 100])
+figure('Name','Initiate Layer 2/3'),yyaxis left,histogram(L23InitiateEventISI,0:.1:2),title('Initiate Layer2/3')%,ylim([0 45])
+yyaxis right,histogram(L23InitiateEventISI(L23InitiateEventISI==-0.5),-1:0.1:0)%,ylim([0 100])
 figure('Name','Run Layer 2/3'),yyaxis left,histogram(L23RunEventISI,0:.1:2),title('Run Layer 2/3')
-yyaxis right,histogram(L23RunEventISI(L23RunEventISI==-0.5),-1:0.1:0),ylim([0 100])
+yyaxis right,histogram(L23RunEventISI(L23RunEventISI==-0.5),-1:0.1:0)%,ylim([0 100])
 
 
 % L5
+count = 1;
 figure,hold on
 L5EventISI = [];
 L5InitiateEventISI = [];
@@ -64,18 +66,19 @@ for ii = 30:64
             %         A = win(win<=1);
             %         B = win(win>1);
             %         ERD(i) = (length(A)-length(B))/(length(A)+length(B));
-            L5EventISI{i} = abs(diff(win));
-            L5RunEventISI{i} = abs(diff(win(win>=1))); % Run Window
-            L5InitiateEventISI{i} = abs(diff(win(win<1))); % Initiate Window
-            if isempty(L5RunEventISI{i}) && isempty(L5InitiateEventISI{i})
-                L5RunEventISI{i} = -.5;
-                L5InitiateEventISI{i} = -.5;
-            elseif isempty(L5RunEventISI{i})
-                L5RunEventISI{i} = -.5;
-            elseif isempty(L5InitiateEventISI{i})
-                L5InitiateEventISI{i} = -.5;
+            L5EventISI{count} = abs(diff(win));
+            L5RunEventISI{count} = abs(diff(win(win>=1))); % Run Window
+            L5InitiateEventISI{count} = abs(diff(win(win<1))); % Initiate Window
+            if isempty(L5RunEventISI{count}) && isempty(L5InitiateEventISI{count})
+                L5RunEventISI{count} = -.5;
+                L5InitiateEventISI{count} = -.5;
+            elseif isempty(L5RunEventISI{count})
+                L5RunEventISI{count} = -.5;
+            elseif isempty(L5InitiateEventISI{count})
+                L5InitiateEventISI{count} = -.5;
             end
         end
+        count = count+1;
     end
 end
 L5EventISI = vertcat(L5EventISI{:});
@@ -83,11 +86,12 @@ L5InitiateEventISI = vertcat(L5InitiateEventISI{:});
 L5RunEventISI = vertcat(L5RunEventISI{:});
 
 figure('Name','Layer 5'),histogram(L5EventISI,0:.1:2)
-figure('Name','Initiate Layer 5'),yyaxis left,histogram(L5InitiateEventISI,0:.1:2),title('Initiate Layer 5'),ylim([0 45])
-yyaxis right,histogram(L5InitiateEventISI(L5InitiateEventISI==-0.5),-1:0.1:0),ylim([0 100])
+figure('Name','Initiate Layer 5'),yyaxis left,histogram(L5InitiateEventISI,0:.1:2),title('Initiate Layer 5')%,ylim([0 45])
+yyaxis right,histogram(L5InitiateEventISI(L5InitiateEventISI==-0.5),-1:0.1:0)%,ylim([0 100])
 figure('Name','Run Layer 5'),yyaxis left,histogram(L5RunEventISI,0:.1:2),title('Run Layer 5')
-yyaxis right,histogram(L5RunEventISI(L5RunEventISI==-0.5),-1:0.1:0),ylim([0 100])
+yyaxis right,histogram(L5RunEventISI(L5RunEventISI==-0.5),-1:0.1:0)%,ylim([0 100])
 % Rest State
+count = 1;
 figure,hold on
 L23RestEventISI = [];
 %L2/3
@@ -100,19 +104,21 @@ for ii = 30:64
             t1 = betaGroupRest(ii).electrode.betaBurst.window(i,1);
             win = t-t1;
             win = win(win<1); %% split in half to make the inwdow the same for run and intitate
-%             plot(win,i*ones(1,length(win)),'.')
+            %             plot(win,i*ones(1,length(win)),'.')
             %Beta Event Interval
-            L23RestEventISI{i} = abs(diff(win));
-            if isempty(L23RestEventISI{i})
-                L23RestEventISI{i} = -.5;
+            L23RestEventISI{count} = abs(diff(win));
+            if isempty(L23RestEventISI{count})
+                L23RestEventISI{count} = -.5;
             end
         end
+        count = count+1;
     end
 end
 L23RestEventISI = vertcat(L23RestEventISI{:});
 figure('Name','Rest Layer 2/3'),yyaxis left,histogram(L23RestEventISI,0:.1:2),title('Rest Layer 2/3')
-yyaxis right,histogram(L23RestEventISI(L23RestEventISI==-0.5),-1:0.1:0),ylim([0 350])
+yyaxis right,histogram(L23RestEventISI(L23RestEventISI==-0.5),-1:0.1:0)%,ylim([0 350])
 % L5
+count = 1;
 figure,hold on
 L5RestEventISI = [];
 for ii = 1:30
@@ -125,16 +131,17 @@ for ii = 1:30
             win = t-t1;
             win = win(win<1); %% split in half to make the inwdow the same for run and intitate
 %             plot(win,i*ones(1,length(win)),'.')
-            L5RestEventISI{i} = abs(diff(win));
-            if isempty(L5RestEventISI{i})
-                L5RestEventISI{i} = -.5;
+            L5RestEventISI{count} = abs(diff(win));
+            if isempty(L5RestEventISI{count})
+                L5RestEventISI{count} = -.5;
             end
         end
+        count = count+1;
     end
 end
 L5RestEventISI = vertcat(L5RestEventISI{:});
 figure('Name','Rest Layer 5'),yyaxis left,histogram(L5RestEventISI,0:.1:2),title('Rest Layer 5')
-yyaxis right,histogram(L5RestEventISI(L5RestEventISI==-0.5),-1:0.1:0),ylim([0 350])
+yyaxis right,histogram(L5RestEventISI(L5RestEventISI==-0.5),-1:0.1:0)%,ylim([0 350])
 %%
 figure,boxplot(L23InitiateEventISI(L23InitiateEventISI>0),'PlotStyle','compact'),ylim([0 1]),title('L23 Initiate')
 figure,boxplot(L23RunEventISI(L23RunEventISI>0),'PlotStyle','compact'),ylim([0 1]),title('L23 Run')
