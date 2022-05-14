0c5ded4ab2b7a76c8e9999703e78f5f2c65ff78e
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-05-13 22:00:50 -0400

    Update BetaEventSI1.m

--- BetaEventSI1.m
+++ BetaEventSI1.m
@@ -1,14 +1,14 @@
 %% Code for SI beta event detection
 % Wavelet of trial # and subsequency filtered Beta LFP
-for i = 14
-signal = TimeFreq.tfRun.betaLFP(:,64,i);
+for i = 1
+signal = TimeFreq.tfRun.betaLFP(:,1,i);
 [wavelet,f] = cwt(signal,1024,'FrequencyLimits',[10 30]);
 blah = abs(wavelet);
 norm = (blah-min(blah,[],'all'))/(max(blah,[],'all')-min(blah,[],'all'));
 figure,imagesc(-1000:1000,f,norm),colormap(jet),colorbar,axis xy,caxis([0.35 .93])
 figure,plot(smoothdata(signal,'gaussian',25)),box off
-refEvent = betaGroup(64).electrode.betaBurst.detectedBeta{i}(:,2);
-refEvent = refEvent- betaGroup(64).electrode.betaBurst.window(i,1);
+refEvent = betaGroup(1).electrode.betaBurst.detectedBeta{i}(:,2);
+refEvent = refEvent-betaGroup(64).electrode.betaBurst.window(i,1);
 refEvent = refEvent.*1024;
 for ii = 1:length(refEvent)
     xline(refEvent(ii),'r--')
