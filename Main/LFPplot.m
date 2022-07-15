15fc4ee53eb93a0e9780e6e6bdc88df3ff21aaf6
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-07-15 17:42:05 -0400

    Update LFPplot.m

--- Main/LFPplot.m
+++ Main/LFPplot.m
@@ -1,14 +1,15 @@
 function LFPplot(LFP)
 Fs = LFP.downSampleFreq;
 start = 200*Fs;
-win = 2*Fs;
+win = 5*Fs;
 stop = start+win-1;
+channel = 64;
 figure(3),set(gcf, 'Position',  [100, 100, 400, 500]),
  %Plot the first 5 seconds of data
-subplot(4,1,1),plot(LFP.times(:,1:win),LFP.LFP(1,start:stop),'k'); title('Raw Data'),box off,ylim([-300 300])
-subplot(4,1,2),plot(LFP.times(:,1:win),LFP.theta_band(:,start:stop),'r');title('\theta Band'),box off,ylim([-100 100])
-subplot(4,1,3),plot(LFP.times(:,1:win),LFP.beta_band(:,start:stop),'m');title('\beta Band'),box off,ylim([-100 100])
-subplot(4,1,4),plot(LFP.times(:,1:win),LFP.gamma_band(:,start:stop),'g');title('\gamma Band'),box off,ylim([-100 100])
+subplot(4,1,1),plot(LFP.times(:,1:win),LFP.LFP(channel,start:stop),'k'); title('Raw Data'),box off,ylim([-300 300])
+subplot(4,1,2),plot(LFP.times(:,1:win),LFP.theta_band(channel,start:stop),'r');title('\theta Band'),box off,ylim([-100 100])
+subplot(4,1,3),plot(LFP.times(:,1:win),LFP.beta_band(channel,start:stop),'m');title('\beta Band'),box off,ylim([-100 100])
+subplot(4,1,4),plot(LFP.times(:,1:win),LFP.gamma_band(channel,start:stop),'g');title('\gamma Band'),box off,ylim([-100 100])
 xlabel('Time (s)'), ylabel('Voltage (\muV)')
 
 figure(4)
