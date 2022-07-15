606990515682d563b8e8082f7daae666201347a6
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-07-15 17:43:15 -0400

    Create behaviorTrig.m

---
+++ Main/behaviorTrig.m
@@ -0,0 +1,14 @@
+function VmTrigAlign = behaviorTrig(out,VmTotal)
+%% Behavior Trig
+% Fix DC offset
+Trig = out.S{5};
+TrigTotal = [];
+for i = 1:size(Trig,2)
+    TrigTotal = [TrigTotal;Trig(:,i)];
+end
+
+flag = find(TrigTotal>2); % Voltage swing from ~0 to ~4 mV;
+startflag = flag(1); % grab first indice
+endflag = flag(end); %grab last indice
+VmTrigAlign = VmTotal(startflag:endflag); % Only include trace when trig starts
+end
