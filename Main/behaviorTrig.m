bc4c31eabeb853ecbe8865eb28b6c013d483dab9
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2022-07-16 17:30:09 -0400

    Update behaviorTrig.m

--- Main/behaviorTrig.m
+++ Main/behaviorTrig.m
@@ -11,4 +11,15 @@ flag = find(TrigTotal>2); % Voltage swing from ~0 to ~4 mV;
 startflag = flag(1); % grab first indice
 endflag = flag(end); %grab last indice
 VmTrigAlign = VmTotal(startflag:endflag); % Only include trace when trig starts
+
+
+%%
+Frame = out.S{3};
+FrameTotal = [];
+for i = 1:size(Frame,2)
+    FrameTotal = [FrameTotal;Frame(:,i)];
 end
+
+
+end
+
