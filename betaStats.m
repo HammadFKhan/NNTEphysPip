b653df9c760deab9adf423e965e6cd847cf09324
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-01-21 18:04:47 -0500

    Update betaStats.m

--- betaStats.m
+++ betaStats.m
@@ -3,12 +3,17 @@ function stats = betaStats(bstats,LFPdepth)
 % [~,l4Idx] = find(LFPdepth>250 & LFPdepth<400);
 [~,l5Idx] = find(LFPdepth>400);
 % bstats = flip(bstats);
+% check to see if all electrodes are there
+idx = find(cellfun(@isempty,bstats(:,2))==1);
 LFPlayers = [repmat({'L23'},length(l23Idx),1);repmat({'L5'},length(l5Idx),1)];
+LFPlayers(idx,:) = [];
 % figure('Name','Beta Duration'),boxplot(bstats(:,1),LFPlayers,'plotstyle','compact');title('Beta Duration')
 figure('Name','Beta AmplitudeNorm'),boxplot(cell2mat(bstats(:,2)),LFPlayers,'plotstyle','traditional'),...
     title('Beta Amplitude Norm'),box off, set(gca,'TickDir','out');
 figure('Name','Beta Amplitude'),boxplot(cell2mat(bstats(:,4)),LFPlayers,'plotstyle','traditional'),...
     title('Beta Amplitude'),box off, set(gca,'TickDir','out');
+
+
 L23betaEvent = cell2mat(bstats(l23Idx,8));
 L5betaEvent = cell2mat(bstats(l5Idx,8));
 betaEventLayers = [repmat({'L23'},length(L23betaEvent),1);repmat({'L5'},length(L5betaEvent),1)];
@@ -16,8 +21,8 @@ figure('Name','Beta AmplitudeSingles'),boxplot(cell2mat(bstats(:,8)),betaEventLa
     title('Beta Amplitude'),box off, set(gca,'TickDir','out');
 
 
-sElectrode = 10; %superficial electrode for beta event rate
-dElectrode = 60; %deep electrode for beta even rate
+sElectrode = (12:20); %superficial electrode for beta event rate
+dElectrode = (42:50); %deep electrode for beta even rate
 L23 = repmat({'Superficial'},length(cell2mat(bstats(sElectrode,7))),1);
 L5 =  repmat({'Deep'},length(cell2mat(bstats(dElectrode,7))),1);
 
