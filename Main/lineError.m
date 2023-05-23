edc8e099a8b23c3e87c01382503d771a159b9862
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-05-22 21:38:01 -0400

    Update lineError.m

--- Main/lineError.m
+++ Main/lineError.m
@@ -5,7 +5,7 @@ elseif strcmp(varargin,'ste')
         errorType = 1;
 else
     warning('No error type given, defaulting to std')
-    errorType = 0;     
+    errorType = 1;     
 end
 
 if errorType == 0
@@ -21,9 +21,9 @@ if errorType == 0
     plot(x, yM, 'k', 'LineWidth', 2);
 else
     yM = mean(y,1);
-    x = x';
+%     x = x';
     error = std(y);
-    error = 6*error/sqrt(length(yM));
+    error = error/sqrt(length(yM));
     curve1 = yM + error;
     curve2 = yM - error;
     x2 = [x, fliplr(x)];
