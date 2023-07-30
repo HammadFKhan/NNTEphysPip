3823489c82e5f985e8431e0d4769a13ac4f516fd
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-07-30 17:37:20 -0400

    Update readLever.m

--- Main/readLever.m
+++ Main/readLever.m
@@ -6,8 +6,7 @@ if isequal(enfile,0)
 else
    disp(['User selected ', fullfile(enpath,enfile)]);
 end
-
-resting_position = 245;
+resting_position = 241;
 flip = 1;
 nlengthBeforePull = round(parameters.windowAfterPull/parameters.ts);
 nlength = round(parameters.windowAfterPull/parameters.ts + parameters.windowAfterPull/parameters.ts + 1);
@@ -18,7 +17,9 @@ Behaviour.time = (B(2:end,2) - B(2,2))/1e6; % time in seconds
 Behaviour.nHit = B(end,3);
 Behaviour.nMiss = B(end,4);
 Behaviour.B = B(2:end,:);
-
+if ~exist('lfpTime','var')
+    lfpTime = Behaviour.time;
+end
 %% Gettting Hit and miss timings
 hitIndex = find(diff(B(:,3)) == 1) + 1;
 hitTime = Behaviour.time(hitIndex);
@@ -52,18 +53,18 @@ for i=1:Behaviour.nHit
     Behaviour.hitTrace(i).t1 = Behaviour.time(Behaviour.hitTrace(i).i1);
     Behaviour.hitTrace(i).t0 = Behaviour.hit(i,2);
     Behaviour.hitTrace(i).t2 = Behaviour.time(Behaviour.hitTrace(i).i2);
-    [Behaviour.hitTrace(i).trace,Behaviour.hitTrace(i).time] = resample(Behaviour.hitTrace(i).rawtrace,Behaviour.hitTrace(i).rawtime,parameters.Fs,'spline');
-    if (size(Behaviour.hitTrace(i).trace,1)<nlength)
-        n1 = size(Behaviour.hitTrace(i).trace,1);
-        n2 = nlength;
-        Behaviour.hitTrace(i).trace = [Behaviour.hitTrace(i).trace;interp1(1:1:n1,Behaviour.hitTrace(i).trace,[n1+1:1:n2],'linear','extrap')'];
-        Behaviour.hitTrace(i).time = [Behaviour.hitTrace(i).time;interp1(1:1:n1,Behaviour.hitTrace(i).time,[n1+1:1:n2],'linear','extrap')'];
-    elseif (size(Behaviour.hitTrace(i).trace,1)>nlength)
-        Behaviour.hitTrace(i).trace(nlength+1:end) = [];
-        Behaviour.hitTrace(i).time(nlength+1:end) = []; 
-    end
-    Behaviour.hitTrace(i).LFPTime = Behaviour.hitTrace(i).time + (Behaviour.hit(i,4)-(nlengthBeforePull*parameters.ts));
-    Behaviour.hitTrace(i).LFPIndex = ([Behaviour.hit(i,3)-nlengthBeforePull:1:nlengthBeforePull+Behaviour.hit(i,3)])';
+%     [Behaviour.hitTrace(i).trace,Behaviour.hitTrace(i).time] = resample(Behaviour.hitTrace(i).rawtrace,Behaviour.hitTrace(i).rawtime,parameters.Fs,'spline');
+%     if (size(Behaviour.hitTrace(i).trace,1)<nlength)
+%         n1 = size(Behaviour.hitTrace(i).trace,1);
+%         n2 = nlength;
+%         Behaviour.hitTrace(i).trace = [Behaviour.hitTrace(i).trace;interp1(1:1:n1,Behaviour.hitTrace(i).trace,[n1+1:1:n2],'linear','extrap')'];
+%         Behaviour.hitTrace(i).time = [Behaviour.hitTrace(i).time;interp1(1:1:n1,Behaviour.hitTrace(i).time,[n1+1:1:n2],'linear','extrap')'];
+%     elseif (size(Behaviour.hitTrace(i).trace,1)>nlength)
+%         Behaviour.hitTrace(i).trace(nlength+1:end) = [];
+%         Behaviour.hitTrace(i).time(nlength+1:end) = []; 
+%     end
+%     Behaviour.hitTrace(i).LFPTime = Behaviour.hitTrace(i).time + (Behaviour.hit(i,4)-(nlengthBeforePull*parameters.ts));
+%     Behaviour.hitTrace(i).LFPIndex = ([Behaviour.hit(i,3)-nlengthBeforePull:1:nlengthBeforePull+Behaviour.hit(i,3)])';
 end
 
 for i=1:Behaviour.nMiss
@@ -77,17 +78,17 @@ for i=1:Behaviour.nMiss
     Behaviour.missTrace(i).t0 = Behaviour.miss(i,2);
     Behaviour.missTrace(i).t2 = Behaviour.time(Behaviour.missTrace(i).i2);
     [Behaviour.missTrace(i).trace,Behaviour.missTrace(i).time] = resample(Behaviour.missTrace(i).rawtrace,Behaviour.missTrace(i).rawtime,parameters.Fs,'spline');
-    if (size(Behaviour.missTrace(i).trace,1)<nlength)
-        n1 = size(Behaviour.missTrace(i).trace,1);
-        n2 = nlength;
-        Behaviour.missTrace(i).trace = [Behaviour.missTrace(i).trace;interp1(1:1:n1,Behaviour.missTrace(i).trace,[n1+1:1:n2],'linear','extrap')'];
-        Behaviour.missTrace(i).time = [Behaviour.missTrace(i).time;interp1(1:1:n1,Behaviour.missTrace(i).time,[n1+1:1:n2],'linear','extrap')'];
-    elseif (size(Behaviour.missTrace(i).trace,1)>nlength)
-        Behaviour.missTrace(i).trace(nlength+1:end) = [];
-        Behaviour.missTrace(i).time(nlength+1:end) = []; 
-    end
-    Behaviour.missTrace(i).LFPTime = Behaviour.missTrace(i).time + (Behaviour.miss(i,4)-(nlengthBeforePull*parameters.ts));
-    Behaviour.missTrace(i).LFPIndex = ([Behaviour.miss(i,3)-nlengthBeforePull:1:nlengthBeforePull+Behaviour.miss(i,3)])';
+%     if (size(Behaviour.missTrace(i).trace,1)<nlength)
+%         n1 = size(Behaviour.missTrace(i).trace,1);
+%         n2 = nlength;
+%         Behaviour.missTrace(i).trace = [Behaviour.missTrace(i).trace;interp1(1:1:n1,Behaviour.missTrace(i).trace,[n1+1:1:n2],'linear','extrap')'];
+%         Behaviour.missTrace(i).time = [Behaviour.missTrace(i).time;interp1(1:1:n1,Behaviour.missTrace(i).time,[n1+1:1:n2],'linear','extrap')'];
+%     elseif (size(Behaviour.missTrace(i).trace,1)>nlength)
+%         Behaviour.missTrace(i).trace(nlength+1:end) = [];
+%         Behaviour.missTrace(i).time(nlength+1:end) = [];
+%     end
+%     Behaviour.missTrace(i).LFPTime = Behaviour.missTrace(i).time + (Behaviour.miss(i,4)-(nlengthBeforePull*parameters.ts));
+%     Behaviour.missTrace(i).LFPIndex = ([Behaviour.miss(i,3)-nlengthBeforePull:1:nlengthBeforePull+Behaviour.miss(i,3)])';
 end
 
 
