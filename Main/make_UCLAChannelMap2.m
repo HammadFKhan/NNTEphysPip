6d45ccbc170754aa594bddf373c69ef86e0dacff
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-07-30 17:37:23 -0400

    Update make_UCLAChannelMap2.m

--- Main/make_UCLAChannelMap2.m
+++ Main/make_UCLAChannelMap2.m
@@ -48,6 +48,6 @@ kcoords = ones(1,64);
 % Now we can save our channel map for the eMouse. 
 
 % would be good to also save the sampling frequency here
-fs = 8192; 
+fs = 10000; 
 
 save(fullfile(fpath, 'UCLA_chanMap_fixed.mat'), 'chanMap', 'connected', 'xcoords', 'ycoords', 'kcoords', 'fs')
