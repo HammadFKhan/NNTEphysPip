e3f3c4a6d54f0be02a7456215af368bca37484ef
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-08-28 19:50:49 -0400

    Create kilosortPrep2.m

---
+++ Main/utilities/kilosortPrep2.m
@@ -0,0 +1,11 @@
+function kilosortPrep2(amplifier_data,path)
+% Kilosort data preprocessing, appends data now instead of overwriting
+datI = int16(amplifier_data);
+kilosortOut = path;
+% [SUCCESS,~,~] = mkdir(kilosortOut,filename(1:end-4));
+% DIR = dir(strcat(kilosortOut,filename(1:end-4)));
+% DIR = DIR.folder;
+fid = fopen(strcat(kilosortOut,'/','kilosort','.bin'),'a');
+fwrite(fid,datI,'int16');
+fclose(fid);
+disp('Kilosort Conversion Successful!')
