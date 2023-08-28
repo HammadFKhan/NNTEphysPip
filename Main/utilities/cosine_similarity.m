a6e252ebd7573d1bc872059e6076f5359d865294
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-08-28 11:41:08 -0400

    Update cosine_similarity.m

--- Main/utilities/cosine_similarity.m
+++ Main/utilities/cosine_similarity.m
@@ -19,18 +19,22 @@ for i = 1:y/bin
     position = bin*(i-1)+1;
     for ii=1:bin
         a(position+(ii-1),i) = 1;
-    end 
+    end
+    if rem(i,10)==0
+        disp(['Vectorizing: ' num2str(i)])
+    end
 end
 vectorized = Spikes*a;
 timei = length(vectorized(1,:));
-sim_index = zeros(timei,timei);
-for j = 1:timei
-    for n = 1:timei
-        sim_index(j,n) = dot(vectorized(:,n),vectorized(:,j))/(norm(vectorized(:,n))*norm(vectorized(:,j)));
-        if isnan(sim_index(j,n))
-            sim_index(j,n) = 0;
-        end
-    end
-end
+sim_index = [];
+% sim_index = zeros(timei,timei);
+% for j = 1:timei
+%     for n = 1:timei
+%         sim_index(j,n) = dot(vectorized(:,n),vectorized(:,j))/(norm(vectorized(:,n))*norm(vectorized(:,j)));
+%         if isnan(sim_index(j,n))
+%             sim_index(j,n) = 0;
+%         end
+%     end
+% end
 % h.YDisplayData = flipud(h.YDisplayData);  % equivalent to 'YDir', 'Reverse'
 end
