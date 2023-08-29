33b0c91662d2dc3ccc66c57f5d13ae23d7aa6004
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2023-08-29 12:16:23 -0400

    Update NginX3.m

--- NginX3.m
+++ NginX3.m
@@ -5,32 +5,18 @@ addpath(genpath('chronux'));
 addpath(genpath('Kilosort'));
 addpath(genpath('npy-matlab'));
 addpath(genpath('spikes-master'));
-IntanConcatenate
-%%
-% Intan = read_Intan_RHD2000_file(); %load intan data
-% ParpoolConfig
-fpath    = Intan.path; % where on disk do you want the analysis? ideally and SSD...
-pathToYourConfigFile = strcat(pwd,'/main/utilities'); % for this example it's ok to leave this path inside the repo, but for your own config file you *must* put it somewhere else!  
-run(fullfile(pathToYourConfigFile, 'config_UCLA64Fprobe.m'))
-% make_UCLAChannelMap2(fpath,s); % Creates channel map for electrode array
-make_UCLAChannelMap64F(fpath)
-kilosortPrep(Intan.allIntan,fpath)
-set(0,'DefaultFigureWindowStyle','normal')
-rez = KilosortAnalysis(fpath,ops);
-% now fire up Phy and check these results. There should still be manual
-% work to be done (mostly merges, some refinements of contaminated clusters). 
-%% AUTO MERGES 
-% after spending quite some time with Phy checking on the results and understanding the merge and split functions, 
-% come back here and run Kilosort's automated merging strategy. This block
-% will overwrite the previous results and python files. Load the results in
-% Phy again: there should be no merges left to do (with the default simulation), but perhaps a few splits
-% / cleanup. On realistic data (i.e. not this simulation) there will be drift also, which will usually
-% mean there are merges left to do even after this step. 
-% Kilosort's AUTO merges should not be confused with the "best" merges done inside the
-% benchmark (those are using the real ground truth!!!)
+% IntanConcatenate legacy version
+ds_filename = intanPreprocessing2;
+%% Kilosort3 
+% load only neccessary variables from memory mapped file
+data = matfile(ds_filename);
+fpath = data.fpath;
+Kilosort264FTestcode
+savepath = fullfile(fpath,['loadme','.mat']);
+save(savepath,'ds_filename');
 %% LFP
 set(0,'DefaultFigureWindowStyle','normal')
-LFP = fastpreprocess_filtering(Intan.allIntan,10000);
+LFP = fastpreprocess_filtering(lfp,Fs);
 LFP = bestLFP(LFP);
 LFP = bandFilter(LFP,'depth'); % Extract LFPs based on 'depth' or 'single'
 %% Parameters 
