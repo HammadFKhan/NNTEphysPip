f9fac17aadbcb7ba90800282c4b64c9b79c92579
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2024-02-26 14:08:32 -0500

    Update colormap_redblackblue.m

--- Main/utilities/colormap_redblackblue.m
+++ Main/utilities/colormap_redblackblue.m
@@ -1,6 +1,6 @@
 function ctotal = colormap_redblackblue()
 % Colormap to make nice red black blue 
-c1 = colormap(flip(hot));
+c1 = colormap(flip(hot)); close all;
 [grad,~]=colorGradient(c1(end,:),[0 0 1],64);
 [grad1,~]=colorGradient([0 0 1],[117 227 255]/256,64);
 ctotal = [colormap(flip(hot(128)));grad;grad1];
