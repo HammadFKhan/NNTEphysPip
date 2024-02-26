a4445019317ec2a73340fe10ed2984d0e0d3a220
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2024-02-26 14:09:42 -0500

    Update customBoxplot.m

--- Main/utilities/customBoxplot.m
+++ Main/utilities/customBoxplot.m
@@ -7,15 +7,15 @@ if ~isempty(varargin) && (strcmp(varargin{1},'Scatter') || strcmp(varargin{1},'s
         scatterOn = 0;
     end
 else
-    scatterOn = 0;
+    scatterOn = 1;
 end
 % Parse input
 if length(size(data))>1
     labels = [];buff = [];
     for i = 1:size(data,2)
         t = data(:,i);
-        buff = [buff;t(t>0)];
-        labels = [labels;repmat({num2str(i)},length(t(t>0)),1)];
+        buff = [buff;t(t~=0)];
+        labels = [labels;repmat({num2str(i)},length(t(t~=0)),1)];
     end
     boxplot(buff,labels), hold on
     h=findobj('LineStyle','--'); set(h, 'LineStyle','-');
@@ -23,9 +23,9 @@ if length(size(data))>1
     if scatterOn
         for i = 1:size(data,2)
             t = data(:,i);
-            buff = [buff;t(t>0)];
+            buff = [buff;t(t~=0)];
             labels = repmat({num2str(i)},length(buff),1);
-            scatter(i*ones(length(t(t>0)),1),t(t>0),'filled','jitter','on','jitterAmount',0.1)
+            scatter(i*ones(length(t(t~=0)),1),t(t~=0),'filled','jitter','on','jitterAmount',0.1)
         end
     end
 else
