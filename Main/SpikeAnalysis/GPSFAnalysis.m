af0996e1bd87573dd572563abf457d9c1adfefe0
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2024-06-18 18:04:02 -0400

    Create GPSFAnalysis.m

---
+++ Main/SpikeAnalysis/GPSFAnalysis.m
@@ -0,0 +1,47 @@
+function Spikes = GPSFAnalysis(Spikes,lfp)
+%% Spike field analysis function for lever task
+% Here we call the generalized phase spike field function to begin analysis
+% across task conditions and cue times of lever task. Along with some basic
+% analysis and quantifications for plotting later down the road
+
+% Grab the localized spike electrode so we can match to GP electrode
+spkChan = arrayfun(@(x) vertcat(x.channelDepth),Spikes.Clusters)';
+precueWin = 1200:1500;
+postcueWin = 1501:1800;
+% We do pre- post- cue reponse across task function. For hits/miss it'll be
+% based on cue response at time 1500. For FA it'll be based on time of
+% movement. Which we will also do for motion initiated hits to serve as a
+% control for the jittered PSTH
+preCuespk = cellfun(@(x) x(:,precueWin), Spikes.PSTH.hit.spks, 'UniformOutput', false);
+preCuexgp = cellfun(@(x) x(:,:,precueWin), lfp.hitxgp, 'UniformOutput', false);
+postCuespk = cellfun(@(x) x(:,postcueWin), Spikes.PSTH.hit.spks, 'UniformOutput', false);
+postCuexgp = cellfun(@(x) x(:,:,postcueWin), lfp.hitxgp, 'UniformOutput', false);
+
+
+[Spikes.SpikeField.precuehit] = GPSpikeField(preCuespk,preCuexgp,spkChan);
+[Spikes.SpikeField.postcuehit] = GPSpikeField(postCuespk,postCuexgp,spkChan);
+
+preCuespk = cellfun(@(x) x(:,precueWin), Spikes.PSTH.miss.spks, 'UniformOutput', false);
+preCuexgp = cellfun(@(x) x(:,:,precueWin), lfp.missxgp, 'UniformOutput', false);
+postCuespk = cellfun(@(x) x(:,postcueWin), Spikes.PSTH.miss.spks, 'UniformOutput', false);
+postCuexgp = cellfun(@(x) x(:,:,postcueWin),lfp.missxgp, 'UniformOutput', false);
+
+[Spikes.SpikeField.precuemiss] = GPSpikeField(preCuespk,preCuexgp,spkChan);
+[Spikes.SpikeField.postcuemiss] = GPSpikeField(postCuespk,postCuexgp,spkChan);
+
+preCuespk = cellfun(@(x) x(:,precueWin), Spikes.PSTH.MIFA.spks, 'UniformOutput', false);
+preCuexgp = cellfun(@(x) x(:,:,precueWin), lfp.MIFAxgp, 'UniformOutput', false);
+postCuespk = cellfun(@(x) x(:,postcueWin), Spikes.PSTH.MIFA.spks, 'UniformOutput', false);
+postCuexgp = cellfun(@(x) x(:,:,postcueWin), lfp.MIFAxgp, 'UniformOutput', false);
+
+[Spikes.SpikeField.precueMIFA] = GPSpikeField(preCuespk,preCuexgp,spkChan);
+[Spikes.SpikeField.postcueMIFA] = GPSpikeField(postCuespk,postCuexgp,spkChan);
+
+preCuespk = cellfun(@(x) x(:,precueWin), Spikes.PSTH.MIHit.spks, 'UniformOutput', false);
+preCuexgp = cellfun(@(x) x(:,:,precueWin), lfp.MIhitxgp, 'UniformOutput', false);
+postCuespk = cellfun(@(x) x(:,postcueWin), Spikes.PSTH.MIHit.spks, 'UniformOutput', false);
+postCuexgp = cellfun(@(x) x(:,:,postcueWin), lfp.MIhitxgp, 'UniformOutput', false);
+
+[Spikes.SpikeField.precueMIHit] = GPSpikeField(preCuespk,preCuexgp,spkChan);
+[Spikes.SpikeField.postcueMIHit] = GPSpikeField(postCuespk,postCuexgp,spkChan);
+end
