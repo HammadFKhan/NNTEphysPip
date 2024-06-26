065c8dd06588d739d3abb299aa41ad79c3af2dc7
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2024-06-26 13:24:26 -0400

    Create leverStats.m

---
+++ Main/BehaviourAnalysis/leverStats.m
@@ -0,0 +1,6 @@
+%% Cooling and eOPN lever behaviour: additional statistics
+% Additional statistics on perturbation experiments regarding normal lever
+% hold and pull time in the same mouse across multiple mice/sessions
+% This is to see if the M2->M1 routing is effecting only RT time to
+% external stimulus or actually changing the learned motor movement. We
+% don't expect a significant difference in lever movement. 
