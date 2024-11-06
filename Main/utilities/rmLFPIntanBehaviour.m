a8bef55244abccffebd5a940f76dace14ea29e1f
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2024-11-06 16:39:53 -0500

    Create rmLFPIntanBehaviour.m

---
+++ Main/utilities/rmLFPIntanBehaviour.m
@@ -0,0 +1,19 @@
+IntanBehaviour.cueHitTrace = rmfield(IntanBehaviour.cueHitTrace, 'rawLFP');
+IntanBehaviour.cueHitTrace = rmfield(IntanBehaviour.cueHitTrace, 'xf');
+IntanBehaviour.cueHitTrace = rmfield(IntanBehaviour.cueHitTrace, 'xgp');
+IntanBehaviour.cueHitTrace = rmfield(IntanBehaviour.cueHitTrace, 'wt');
+
+IntanBehaviour.cueMissTrace = rmfield(IntanBehaviour.cueMissTrace, 'rawLFP');
+IntanBehaviour.cueMissTrace = rmfield(IntanBehaviour.cueMissTrace, 'xf');
+IntanBehaviour.cueMissTrace = rmfield(IntanBehaviour.cueMissTrace, 'xgp');
+IntanBehaviour.cueMissTrace = rmfield(IntanBehaviour.cueMissTrace, 'wt');
+
+IntanBehaviour.MIHitTrace = rmfield(IntanBehaviour.MIHitTrace, 'rawLFP');
+IntanBehaviour.MIHitTrace = rmfield(IntanBehaviour.MIHitTrace, 'xf');
+IntanBehaviour.MIHitTrace = rmfield(IntanBehaviour.MIHitTrace, 'xgp');
+IntanBehaviour.MIHitTrace = rmfield(IntanBehaviour.MIHitTrace, 'wt');
+
+IntanBehaviour.MIFATrace = rmfield(IntanBehaviour.MIFATrace, 'rawLFP');
+IntanBehaviour.MIFATrace = rmfield(IntanBehaviour.MIFATrace, 'xf');
+IntanBehaviour.MIFATrace = rmfield(IntanBehaviour.MIFATrace, 'xgp');
+IntanBehaviour.MIFATrace = rmfield(IntanBehaviour.MIFATrace, 'wt');
