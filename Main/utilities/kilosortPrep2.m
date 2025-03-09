d2d909a48d3234f9e40e58430815a864a2ef2d9d
HammadFKhan <56140216+HammadFKhan@users.noreply.github.com>
2025-03-09 17:49:03 -0400

    Update kilosortPrep2.m

--- Main/utilities/kilosortPrep2.m
+++ Main/utilities/kilosortPrep2.m
@@ -4,11 +4,11 @@ if ~exist('kname','var') || isempty(kname)
     kname = 'kilosort';
 end
 datI = int16(amplifier_data);
-kilosortOut = path;
+kilosortOut = [path,'\',kname(10:end)];
 % [SUCCESS,~,~] = mkdir(kilosortOut,filename(1:end-4));
 % DIR = dir(strcat(kilosortOut,filename(1:end-4)));
 % DIR = DIR.folder;
-fid = fopen(strcat(kilosortOut,'/',kname,'.bin'),'a');
+fid = fopen(strcat(kilosortOut,'\',kname,'.bin'),'a');
 fwrite(fid,datI,'int16');
 fclose(fid);
 disp('Kilosort Conversion Successful!')
